c770c679a0d3166d6e058c69a45ec4ad0bf3a491
Moshe Ofer <107894139+MosheOfer1@users.noreply.github.com>
2023-03-28 19:18:15 +0300

    Update card.cpp

--- sources/card.cpp
+++ sources/card.cpp
@@ -1,14 +1,40 @@
 #include "card.hpp"
 using namespace ariel;
 
-Card::Card(string str, int num) : suit(str), rank(num) 
+Card::Card(std::string suit, int rank)
 {
-    // To be implemented
+    m_suit = suit;
+    m_rank = rank;
 }
 // Card::~Card()
 // {
 //     // To be implemented
 // }
-string Card::get_suit() { return suit; }
-int Card::get_rank() { return rank; }
-
+std::string Card::getSuit() const
+{
+    return m_suit;
+}
+int Card::getRank()
+{
+    return m_rank;
+} 
+std::string Card::getRankStr() const
+{
+    if (m_rank < 11 && m_rank != 1)
+    {
+        return std::to_string(m_rank);
+    }
+    else if (m_rank == 11)
+    {
+        return "Jack";
+    }
+    else if (m_rank == 12)
+    {
+        return "Queen";
+    }
+    else if (m_rank == 13)
+    {
+        return "King";
+    }
+    return "Ace";
+}
